4247ad3df6d9260d5b09eb5bfa5b4b0d1b856d1a
Sierra Clibourne <clibo100@mail.chapman.edu>
2018-09-10 14:31:17 -0700

    started snake script

---
+++ Keep Snaking/Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour {
+
+    public List<Transform> BodyParts = new List<Transform>();
+
+    public float mindistance = 0.25f;
+
+    public float speed = 1;
+    public float rotationspeed = 50;
+
+    public GameObject bodyprefab;
+
+    private float distance;
+    private Transform currBodyPart;
+    private Transform prevBodyPart;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddBodyPart()
+    {
+        Transform newpart = (Instantiate(bodyprefab, BodyParts[BodyParts.Count - 1].position, BodyParts[BodyParts.Count - 1].rotation) as GameObject).transform;
+    }
+}
